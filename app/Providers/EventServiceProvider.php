31c5caaf57e34773a23dca7ea2d494f1c052346c
CRRuiz <rne.eclipse@gmail.com>
2016-04-19 11:39:11 -0600

    Laravel
    Inicio

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace sialas\Providers;
+
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'sialas\Events\SomeEvent' => [
+            'sialas\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
+
+        //
+    }
+}

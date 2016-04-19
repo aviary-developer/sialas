3fdf2bd7cb00204c9d6feb858be743487da07355
HqzMer <opsa.al.el@gmail.com>
2016-04-19 11:54:10 -0600

    codigo
    df

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'jO8RKYukjRTzPWQUgnWo7XDZ7ycW2lCe'),
 
     'cipher' => 'AES-256-CBC',
 

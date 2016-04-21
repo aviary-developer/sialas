3b114f5ecde4803943bc9fa2c6979da1643fdee5
RSibrian <blanky.r.sibrian@gmail.com>
2016-04-21 09:45:48 -0600

    formulario
    se realizo las vistas de la tabla marcas

--- resources/views/Marcas/create.blade.php
+++ resources/views/Marcas/create.blade.php
@@ -1,6 +1,7 @@
 @extends('welcome')
 @section('layout')
 {!! Form::open(['route'=>'marcas.store','methoh'=>'POST'])!!}
-
+@include('marcas.Formularios.formularios')
+{!!Form::submit('Guardar')!!}
 {!!Form::close()!!}
 @stop

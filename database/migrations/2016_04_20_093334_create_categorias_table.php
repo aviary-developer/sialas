bae37346a81eb054f4e30cf68910281f1e63ba54
CRRuiz <rne.eclipse@gmail.com>
2016-04-25 09:24:06 -0600

    Estado en categoria
    Se agrego la estado a la tabla categoria, hay que rehacer la tabla para
    migrar correctamente

--- database/migrations/2016_04_20_093334_create_categorias_table.php
+++ database/migrations/2016_04_20_093334_create_categorias_table.php
@@ -15,6 +15,7 @@ class CreateCategoriasTable extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre',30);
+            $table->boolean('estado')->default(true);
             $table->timestamps();
         });
     }

a1d42712969395dfa7ac6a180ece675981b954e4
ngrd94 <ingridayala_94@hotmail.com>
2016-04-25 10:20:51 -0600

    !!Modificación en routes
    Las rutas que se crean deben ir abajo del principal por lo que he dado
    espacio a cada ruta para no tener problemas cuando se agreguen ejemplo
    ruta marcas esta en linea 18 si se agregan otras rutas para nuevas
    funciones deben ir en las lineas 19-27

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,19 +10,60 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::resource('marcas','MarcasController');
-
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::resource('marcas','MarcasController');
+
+
+
+
+
+
+
+
+
 Route::resource('categorias', 'CategoriasController');
+
+
+
+
+
+
+
+
 
 Route::resource('servicios','ServiciosController');
 
+
+
+
+
+
+
+
+
 Route::resource('users','UsersController');
 
+
+
+
+
+
+
+
+
 Route::resource ('clientes', 'ClientesController');
+
+
+
+
+
+
+
+
 
 Route::resource('cajas', 'CajasController');
 Route::match(['get','post'],'/darAltaCaja/{id}','CajasController@darAlta');

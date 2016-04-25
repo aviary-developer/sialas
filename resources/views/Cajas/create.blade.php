b4775f39812ef68d36175d056fe752384f4e98a0
HqzMer <opsa.al.el@gmail.com>
2016-04-25 10:22:43 -0600

    Agregue diseño a create

--- resources/views/Cajas/create.blade.php
+++ resources/views/Cajas/create.blade.php
@@ -1,7 +1,33 @@
 @extends('welcome')
 @section('layout')
+  <div class="launcher">
+    <div class="lfloat"></div>
+    <div class="tooltip">
+      <a href="#">
+        <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      </a>
+      <span class="tooltiptext">Atras</span>
+    </div>
+    <div class="tooltip">
+      <a href="#">
+        <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      </a>
+      <span class="tooltiptext">Nuevo</span>
+    </div>
+    <div class="tooltip">
+      <a href="#">
+        <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      </a>
+      <span class="tooltiptext">Ayuda</span>
+    </div>
+  </div>
+  <div class="panel">
+    <div class="enc">
+      <h2>Categorias</h2>
+    </div>
 {!! Form::open(['route'=>'cajas.store','methoh'=>'POST'])!!}
 @include('cajas.Formularios.formulario')
 {!!Form::submit('Guardar')!!}
 {!!Form::close()!!}
+</div>
 @stop

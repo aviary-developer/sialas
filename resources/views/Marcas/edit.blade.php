d61c5b3949d8f54ff9b0bb3cbd1b51e3ca8eb90d
RSibrian <blanky.r.sibrian@gmail.com>
2016-04-27 10:24:22 -0600

    Dar y quitar de baja
    todo lo relacionado a la tabla de marcas

--- resources/views/Marcas/edit.blade.php
+++ resources/views/Marcas/edit.blade.php
@@ -1,7 +1,34 @@
-@extends('welcome')
-@section('layout')
-  {!! Form::model($marcas,['route'=>['marcas.update',$marcas->id],'method'=>'PUT']) !!}
+
+@extends ('welcome')
+@section ('layout')
+  <div class="launcher">
+    <div class="lfloat"></div>
+    <div class="tooltip">
+      <a href="#">
+        <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      </a>
+      <span class="tooltiptext">Atras</span>
+    </div>
+    <div class="tooltip">
+      <a href="#">
+        <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      </a>
+      <span class="tooltiptext">Nuevo</span>
+    </div>
+    <div class="tooltip">
+      <a href="#">
+        <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      </a>
+      <span class="tooltiptext">Ayuda</span>
+    </div>
+  </div>
+  <div class="panel">
+    <div class="enc">
+      <h2>Marcas</h2>
+    </div>
+    {!! Form::model($marcas,['route'=>['marcas.update',$marcas->id],'method'=>'PUT']) !!}
   @include('Marcas.Formularios.formularios')
   {!! Form::submit('Actualizar') !!}
   {!!Form::close()!!}
+  </div>
 @stop
--- resources/views/Marcas/index.blade.php
+++ resources/views/Marcas/index.blade.php
@@ -1,22 +1,32 @@
 @extends('welcome')
 @section('layout')
+@if(Session::has('mensaje'))
+<?php $men=Session::get('mensaje');
+echo "<script>swal('$men', 'Click al botón!', 'success')</script>";?>
+@endif
 <div class="launcher">
   <div class="lfloat"></div>
   <div class="tooltip">
     <a href="#">
-      <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      <img src={!! asset('/img/WB/atr.svg') !!} alt="" class="circ"/>
     </a>
     <span class="tooltiptext">Atras</span>
   </div>
   <div class="tooltip">
     <a href="#">
-      <img id= "im" src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ" onclick="activo('block','none','tt','im')"/>
+      <img id= "im" src={!! asset('/img/WB/pre.svg') !!} alt="" class="circ" onclick="activo('block','none','tt','im')"/>
     </a>
-    <span class="tooltiptext" id="tt">Nuevo</span>
+    <span class="tooltiptext" id="tt">Papelera</span>
   </div>
   <div class="tooltip">
     <a href="#">
-      <img src={!! asset('/img/WB/general/circ.svg') !!} alt="" class="circ"/>
+      <img src={!! asset('/img/WB/imp.svg') !!} alt="" class="circ"/>
+    </a>
+    <span class="tooltiptext">Reporte</span>
+  </div>
+  <div class="tooltip">
+    <a href="#">
+      <img src={!! asset('/img/WB/ayu.svg') !!} alt="" class="circ"/>
     </a>
     <span class="tooltiptext">Ayuda</span>
   </div>
@@ -30,54 +40,42 @@
   <center>
     <table id="block">
       <tr>
-        <th>
-          Id
-        </th>
-        <th>
-          Nombre
-        </th>
-        <th>
-          Acciones
-        </th>
+        <th> Id</th>
+        <th> Nombre</th>
+        <th colspan="3">Acciones</th>
       </tr>
-      @foreach($marcas as $c)
+      <?php $a=1; ?>
+      @foreach($marcasActivas as $c)
         <tr>
-          <td>
-            {{$c->id}}
-          </td>
-          <td>
-            {{$c->nombre}}
-          </td>
+          <td>{{$c->id}} </td>
+          <td> {{$c->nombre}}</td>
           <td>
             {!! link_to_route('marcas.edit', $title = "Editar", $parameters=$c->id, $attributes=[]) !!}
           </td>
+        <td>@include('Marcas.Formularios.darDeBaja')</td>
+        <td>{!! link_to_route("marcas.show", $title = "Ver más...", $parameters=$c->id, $attributes=[]) !!}
         </tr>
+    <?php $a=$a+1; ?>
       @endforeach
     </table>
     <table id="none">
       <tr>
-        <th>
-          Id
-        </th>
-        <th>
-          Nombre
-        </th>
-        <th>
-          Acciones
-        </th>
+        <th> Id</th>
+        <th> Nombre</th>
+        <th colspan="3">Acciones</th>
       </tr>
-      @foreach($marcas as $c)
+      <?php $a=1; ?>
+      @foreach($marcasInactivas as $c)
         <tr>
-          <td>
-            {{$c->id}}
-          </td>
-          <td>
-            {{$c->nombre}}
-          </td>
+          <td>{{$c->id}} </td>
+          <td> {{$c->nombre}}</td>
           <td>
             {!! link_to_route('marcas.edit', $title = "Editar", $parameters=$c->id, $attributes=[]) !!}
           </td>
-        </tr>
+        <td>@include('Marcas.Formularios.darDeAlta')</td>
+        <td>{!! link_to_route("marcas.show", $title = "Ver más...", $parameters=$c->id, $attributes=[]) !!}
+      </tr>
+    <?php $a=$a+1; ?>
       @endforeach
     </table>
   </center>

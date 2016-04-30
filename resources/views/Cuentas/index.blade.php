0ac9948cf01069c20c97c5351378e7ea6aae1b7b
ngrd94 <ingridayala_94@hotmail.com>
2016-04-29 20:21:54 -0600

    Formato para mostrar catalogo

--- resources/views/Cuentas/index.blade.php
+++ resources/views/Cuentas/index.blade.php
@@ -8,6 +8,14 @@
   </div>
   <div>
 @foreach($cuentas as $c)
+<?php  $letras=strlen($c->codigo);?>
+@if($letras==1)
+	<br>
+@endif
+@for($i=0;$i<$letras*2;$i++)
+	&nbsp;
+@endfor
+	
 	{{$c->codigo}}&nbsp;
 	{{$c->cuenta}}<br>
 @endforeach

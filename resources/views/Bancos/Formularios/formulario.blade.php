f3128982a8683c41fb95f74dd89f0ec99880985c
HqzMer <opsa.al.el@gmail.com>
2016-05-11 21:41:23 -0600

    Cascarón de Bancos

---
+++ resources/views/Bancos/Formularios/formulario.blade.php
@@ -0,0 +1,5 @@
+
+{!!Form::label('LNombre','Nombre:')!!}
+{!!Form::text('nombre',null,['placeholder'=>'Ingrese el nombre','focusable'])!!}
+{!!Form::label('LUbicacion','Ubicacion:')!!}
+{!!Form::text('ubicacion',null,['placeholder'=>'Ingrese la ubicación'])!!}
---
+++ resources/views/Bancos/show.blade.php
@@ -0,0 +1,64 @@
+@extends('welcome')
+@section('layout')
+@if(Session::has('mensaje'))
+<?php $men=Session::get('mensaje');
+echo "<script>swal('$men', 'Click al botón!', 'success')</script>";?>
+@endif
+<div class="launcher">
+  <div class="lfloat"></div>
+  <div class="tooltip">
+    <a href={!! asset('/cajas') !!}>
+      <img src={!! asset('/img/WB/atr.svg') !!} alt="" class="circ"/>
+    </a>
+    <span class="tooltiptext">Atras</span>
+  </div>
+  <div class="tooltip">
+    <a href={!! asset('/cajas/'.$caja->id.'/edit') !!}>
+      <img src={!! asset('/img/WB/edi.svg') !!} alt="" class="circ"/>
+    </a>
+    <span class="tooltiptext">Editar</span>
+  </div>
+  @if($caja->estado == 1)
+    <div class="tooltip">
+        @include('Cajas.Formularios.darDeBaja')
+      <span class="tooltiptext">Papelera</span>
+    </div>
+  @else
+    <div class="tooltip">
+        @include('Cajas.Formularios.darDeAlta')
+      <span class="tooltiptext">Activar</span>
+    </div>
+  @endif
+  <div class="tooltip">
+    <a href="#">
+      <img src={!! asset('/img/WB/imp.svg') !!} alt="" class="circ"/>
+    </a>
+    <span class="tooltiptext">Reporte</span>
+  </div>
+  <div class="tooltip">
+    <a href="#">
+      <img src={!! asset('/img/WB/ayu.svg') !!} alt="" class="circ"/>
+    </a>
+    <span class="tooltiptext">Ayuda</span>
+  </div>
+</div>
+<div class="panel">
+  <div class="enc">
+    <h2>Cajas</h2>
+    <h3 id="txt"> |{{$caja->nombre}}</h3>
+  </div>
+  <div class="datos">
+    <pre>Identificador:&#09;&#09;&#09;&#09;<span>{!!$caja->id !!}</span></pre>
+    <pre>Nombre:&#09;&#09;&#09;&#09;&#09;<span>{{ $caja->nombre }}</span></pre>
+    <pre>Nombre:&#09;&#09;&#09;&#09;&#09;<span>{{ $caja->ubicacion }}</span></pre>
+    @if($caja->estado == 1)
+      <?php $var = 'Activo'?>
+    @else
+      <?php $var = 'En papelera'?>
+    @endif
+    <pre>Estado:&#09;&#09;&#09;&#09;&#09;<span>{{$var}}</span></pre>
+    <pre>Fecha de creación:&#09;&#09;&#09;<span>{{$caja->created_at->format('d-m-Y g:i:s a')}} </span></pre>
+    <pre>Fecha de última edición:&#09;&#09;<span>{{$caja->updated_at->format('d-m-Y g:i:s a')}} </span></pre>
+  </div>
+</div>
+@stop

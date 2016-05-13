b806b8a550f81acb472a9083041fefb68764e90d
ngrd94 <ingridayala_94@hotmail.com>
2016-05-13 09:36:27 -0600

    Personalizar mensajes de validacion

--- resources/views/servicios/formularios/formulario.blade.php
+++ resources/views/servicios/formularios/formulario.blade.php
@@ -1,8 +1,21 @@
+<div class="alerta-errores">
+	@foreach ($errors->get('nombre') as $error)
+		<br>{{$error}}
+	@endforeach
+</div>
 {!!Form::label('lnombre','Tipo de servicio:')!!}
 {!!Form::text('nombre',null,['placeholder'=>'Servicio'])!!}
-
+<div class="alerta-errores">
+	@foreach ($errors->get('proveedor') as $error)
+		<br>{{$error}}
+	@endforeach
+</div>
 {!!Form::label('lproveedor','Proveedor:')!!}	
 {!!Form::text('proveedor',null,['placeholder'=>'Proveedor'])!!}	
-
+<div class="alerta-errores">
+	@foreach ($errors->get('n_recibo') as $error)
+		<br>{{$error}}
+	@endforeach
+</div>
 {!!Form::label('lrecibo','Número de factura:')!!}	
 {!!Form::text('n_recibo',null,['placeholder'=>'Recibo'])!!}

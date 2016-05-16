33bdd41c8c95c60738910b23d4b4cbe1a44d5b1b
ngrd94 <ingridayala_94@hotmail.com>
2016-05-16 08:43:45 -0600

    mensajes de request

--- app/Http/Requests/ServiciosRequest.php
+++ app/Http/Requests/ServiciosRequest.php
@@ -24,9 +24,22 @@ class ServiciosRequest extends Request
     public function rules()
     {
         return [
-            'nombre'=>'required ',
-            'proveedor'=>'required',
-            'n_recibo'=>'required',
+            'nombre'=>'required | min:3',
+            'proveedor'=>'required | min:5',
+            'n_recibo'=>'required | min:6',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nombre.required' => 'El campo tipo de servicio es obligatorio',
+            'nombre.min' => 'Tipo de servicio debe contener al menos 3 caracteres',
+
+            'proveedor.min' => 'Proveedor debe contener al menos 4 caracteres',
+
+            'n_recibo.required'  => 'El campo número de factura es obligatorio',
+            'n_recibo.min' => 'Número de factura debe contener al menos 6 caracteres',
         ];
     }
 }

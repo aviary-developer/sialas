1e9b8f2065573b4a1b6f702bf355de96448267cb
RSibrian <blanky.r.sibrian@gmail.com>
2016-05-17 09:29:19 -0600

    validadacionProductos
    la validacion ha quedado ha medias, porque trabajare con otras tablas

---
+++ app/Http/Requests/ProductosRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace sialas\Http\Requests;
+
+use sialas\Http\Requests\Request;
+
+class ProductosRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nombre'=>'required | min:3',
+        
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nombre.required' => 'El campo tipo de servicio es obligatorio',
+            'nombre.min' => 'Tipo de servicio debe contener al menos 3 caracteres',
+
+            'proveedor.min' => 'Proveedor debe contener al menos 4 caracteres',
+
+            'n_recibo.required'  => 'El campo número de factura es obligatorio',
+            'n_recibo.min' => 'Número de factura debe contener al menos 6 caracteres',
+        ];
+    }
+}

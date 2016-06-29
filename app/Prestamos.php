7f7f9043648c4f74ba5c548c0efd3cc2c15cb567
RSibrian <blanky.r.sibrian@gmail.com>
2016-06-29 08:51:39 -0600

    trabajando prestamos

--- app/Prestamos.php
+++ app/Prestamos.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prestamos extends Model
 {
-    //
+    protected $fillable = ['banco_id','caja_id','monto','interes','num_cuotas','val_cuotas','cuenta','dia_pago','desposito','banconombre','garantia'];
 }

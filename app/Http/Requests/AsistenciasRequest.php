4cc2b496667de4c2ca73165cc1c847fcb0209631
dianavelasquez <dya_re@hotmail.com>
2016-07-18 14:28:59 -0600

    validacion
    fgkakdfjg

---
+++ app/Http/Requests/AsistenciasRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace sialas\Http\Requests;
+
+use sialas\Http\Requests\Request;
+
+class AsistenciasRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+
+    public function rules()
+    {
+        return [
+        
+        ];
+    }
+}
---
+++ app/Http/Requests/BancomobiliariosRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace sialas\Http\Requests;
+
+use sialas\Http\Requests\Request;
+
+class BancomobiliariosRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        'cantidad'=>'required | min:1'
+        'detalle'=>'required | min:6',
+        'cheque'=>'required | min:6 | max:6',
+        ];
+    }
+}

1d1cfc2c6911bf2dbb6114439c6176e1dfa3ac39
RSibrian <blanky.r.sibrian@gmail.com>
2016-07-19 11:54:17 -0600

    arreglando

--- resources/views/Servicios/show.blade.php
+++ resources/views/Servicios/show.blade.php
@@ -18,6 +18,12 @@ echo "<script>swal('$men', 'Click al botón!', 'success')</script>";?>
     </a>
     <span class="tooltiptext">Editar</span>
   </div>
+  <div class="tooltip">
+    <a href={!! asset('/pagoservicios/crear/'.$s->id) !!}>
+      <img src={!! asset('/img/WB/vend.svg') !!} alt="" class="circ"/>
+    </a>
+    <span class="tooltiptext">Pago</span>
+  </div>
   @if($s->estado == 1)
     <div class="tooltip">
         @include('Servicios.Formularios.darDeBaja')
@@ -48,19 +54,99 @@ echo "<script>swal('$men', 'Click al botón!', 'success')</script>";?>
     <h2></h2>
     <h3 id="txt"> |{{$s->nombre}}</h3>
   </div>
-  <div class="datos">
-    <pre>Identificador:&#09;&#09;&#09;&#09;<span>{!!$s->id !!}</span></pre>
-    <pre>N° de recibo:&#09;&#09;&#09;&#09;<span>{{ $s->n_recibo }}</span></pre>
-    <pre>Nombre:&#09;&#09;&#09;&#09;&#09;<span>{{ $s->nombre }}</span></pre>
-    <pre>Proveedor:&#09;&#09;&#09;&#09;<span>{{ $s->proveedor }}</span></pre>
-    @if($s->estado == 1)
-      <?php $var = 'Activo'?>
-    @else
-      <?php $var = 'En papelera'?>
-    @endif
-    <pre>Estado:&#09;&#09;&#09;&#09;&#09;<span>{{$var}}</span></pre>
-    <pre>Fecha de creación:&#09;&#09;&#09;<span>{{$s->created_at->format('d-m-Y g:i:s a')}} </span></pre>
-    <pre>Fecha de última edición:&#09;&#09;<span>{{$s->updated_at->format('d-m-Y g:i:s a')}} </span></pre>
-  </div>
+  <center>
+    <div class="tpcontenido">
+      <ul class="supernav">
+        <li id ="luno" class="activ" onclick="cambio('uno','luno')">Datos</li>
+        <li id="ldos" onclick="cambio('dos','ldos')">Pagos</li>
+        <li id="ltres" onclick="cambio('tres','ltres')">Estado</li>
+
+      </ul>
+      <div class="tabs ve" id="uno">
+        <!---->
+        <div class="enc">
+          <h3 id="txt">Datos</h3>
+        </div>
+        <div class="srow">
+          <span>Código</span>
+          <span>{!! $s->id !!}</span>
+        </div>
+        <div class="srow">
+          <span>Nombre</span>
+          <span>{!! $s->nombre !!}</span>
+        </div>
+        <div class="srow">
+          <span>Proveedor</span>
+          <span>{!! $s->proveedor !!}</span>
+        </div>
+        <div class="srow">
+          <span>Numero de Recibo</span>
+          <span>{!! $s->n_recibo !!}</span>
+        </div>
+      </div>
+
+
+      <!----->
+      <div class="tabs oc" id="dos">
+        <div class="enc">
+          <h3 id="txt">Pagos</h3>
+        </div>
+          <br>
+          <table>
+            <tr>
+              <th>#</th>
+
+              <th>Pago</th>
+              <th>monto</th>
+              <th>Factura</th>
+
+            </tr>
+            <?php $a = 1; ?>
+            @foreach($p as $k)
+              <tr>
+                <td><center>{{$a}}</center></td>
+                @if($k->caja_id==null)
+                <td><center>{{$k->nombreBanco($k->banco_id)}}</center></td>
+                @else
+                <td><center>{{$k->nombreCaja($k->caja_id)}}</center></td>
+                @endif
+                <td><center>{{'$ '.number_format($k->monto,2)}}</center></td>
+                <td><center>{{$k->factura}}</center></td>
+
+              </tr>
+              <?php $a++; ?>
+            @endforeach
+          </table>
+
+          <br>
+      </div>
+
+
+
+        <!---->
+        <div class="tabs oc" id="tres">
+          <div class="enc">
+            <h3 id="txt">Estado</h3>
+          </div>
+          <div class="srow">
+            @if($s->estado == 1)
+              <?php $var = 'Activo'?>
+            @else
+              <?php $var = 'En papelera'?>
+            @endif
+            <span>Estado</span>
+            <span>{!! $var !!}</span>
+          </div>
+          <div class="srow">
+            <span>Fecha de creación</span>
+            <span>{!! $s->created_at->format('d-m-Y g:i:s a') !!}</span>
+          </div>
+          <div class="srow">
+            <span>Fecha de última edición</span>
+            <span>{!! $s->updated_at->format('d-m-Y g:i:s a') !!}</span>
+          </div>
+        </div>
+      </div>
+    </center>
 </div>
 @stop
